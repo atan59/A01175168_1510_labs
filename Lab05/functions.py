146c45ba09398cea38f313d8463accf8aa77e690
atan59 <atan59@my.bcit.ca>
2019-10-07 15:35:28 -0700

    Finished docstring for generate_consonant function in functions.py.

--- Lab05/functions.py
+++ Lab05/functions.py
@@ -67,6 +67,11 @@ def generate_vowel():
 
 
 def generate_consonant():
+    """
+    Generate a random consonant.
+
+    :return: a string
+    """
     consonant_list = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x',
                       'y', 'z']
 

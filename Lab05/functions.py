77fa37044142c3b0350aa287519c8e65521fd72c
atan59 <atan59@my.bcit.ca>
2019-10-07 14:37:12 -0700

    Finished choose_inventory function in functions.py.

--- Lab05/functions.py
+++ Lab05/functions.py
@@ -13,7 +13,22 @@ def roll_die(number_of_rolls, number_of_sides):
 
 
 def choose_inventory(inventory, selection):
-    pass
+    if (inventory == []) and (selection == 0):
+        result = []
+    elif selection < 0:
+        print("You can't make a negative selection!")
+        result = []
+    elif selection > len(inventory):
+        print("Your selection is greater than the amount of items in the inventory.")
+        result = sorted(inventory)
+    elif selection == len(inventory):
+        result = sorted(inventory)
+    else:
+        result = []
+        for i in range(0, selection):
+            result.append(random.sample(inventory, selection))
+        result = sorted(result)
+    return result
 
 
 def generate_vowel():

c001b1033362ab6559ed6ea5144cd0258fe2c974
atan59 <atan59@my.bcit.ca>
2019-10-07 15:09:08 -0700

    Finished create_character function in functions.py.

--- Lab05/functions.py
+++ Lab05/functions.py
@@ -1,5 +1,6 @@
 """A module of functions for Lab 5."""
 import random
+import doctest
 
 
 def roll_die(number_of_rolls, number_of_sides):
@@ -7,6 +8,7 @@ def roll_die(number_of_rolls, number_of_sides):
         total = 0
     else:
         total = 0
+
         for rolls in range(number_of_rolls):
             total += random.randint(1, number_of_sides + 1)
     return total
@@ -25,14 +27,17 @@ def choose_inventory(inventory, selection):
         result = sorted(inventory)
     else:
         result = []
+
         for i in range(0, selection):
             result.append(random.sample(inventory, selection))
+
         result = sorted(result)
     return result
 
 
 def generate_vowel():
     vowel_list = ['a', 'e', 'i', 'o', 'u', 'y']
+
     vowel = vowel_list[random.randint(0, len(vowel_list) - 1)]
     return vowel
 
@@ -40,12 +45,14 @@ def generate_vowel():
 def generate_consonant():
     consonant_list = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x',
                       'y', 'z']
+
     consonant = consonant_list[random.randint(0, len(consonant_list) - 1)]
     return consonant
 
 
 def generate_name(syllables):
     name = ''
+
     for i in range(0, syllables):
         syllable = generate_consonant() + generate_vowel()
         name += syllable
@@ -53,8 +60,22 @@ def generate_name(syllables):
 
 
 def create_character(name_length):
-    pass
+    if name_length < 0:
+        print("This is an invalid name length.")
+    else:
+        character_stats = []
+        attribute_names = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma']
+
+        character_stats.append(generate_name(name_length))
+
+        for i in range(0, 6):
+            attribute = [attribute_names[i], roll_die(3, 6)]
+            character_stats.append(attribute)
+    return character_stats
 
 
 def print_character(character):
     pass
+
+if __name__ = "__main__":
+    doctest.testmod()

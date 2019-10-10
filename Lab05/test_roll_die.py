74790d7b73240bb9d965dbb037f4d97d212ac396
atan59 <atan59@my.bcit.ca>
2019-10-09 23:52:51 -0700

    Completed unittest for roll_die function.

--- Lab05/test_roll_die.py
+++ Lab05/test_roll_die.py
@@ -1,6 +1,53 @@
 from unittest import TestCase
+from unittest.mock import patch
+from Lab05.lab05 import roll_die
 
 
 class TestRoll_die(TestCase):
-    def test_roll_die(self):
-        self.fail()
+    @patch('random.randint', return_value=1)
+    def test_roll_die_negative_rolls(self, mock_randint):
+        actual = roll_die(-1, 6)
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_negative_sides(self, mock_randint):
+        actual = roll_die(3, -1)
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_zero_rolls(self, mock_randint):
+        actual = roll_die(0, 6)
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_zero_sides(self, mock_randint):
+        actual = roll_die(3, 0)
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_one_roll(self, mock_randint):
+        actual = roll_die(1, 6)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_one_side(self, mock_randint):
+        actual = roll_die(3, 1)
+        expected = 3
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_one_roll_one_side(self, mock_randint):
+        actual = roll_die(1, 1)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_roll_die_positive_rolls_positive_sides(self, mock_randint):
+        actual = roll_die(3, 6)
+        expected = 3
+        self.assertEqual(expected, actual)

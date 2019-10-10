8bf5ea61794ccaba70e19b6e893ed352577a3ee5
atan59 <atan59@my.bcit.ca>
2019-10-10 00:19:57 -0700

    Finished unittest for generate_name.

--- Lab05/test_generate_name.py
+++ Lab05/test_generate_name.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+from unittest.mock import patch
+from Lab05.lab05 import generate_name
 
 
 class TestGenerate_name(TestCase):
-    def test_generate_name(self):
-        self.fail()
+    @patch('random.randint', return_value=0)
+    def test_generate_vowel_zero(self, mock_randint):
+        actual = generate_name(0)
+        expected = ''
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=0)
+    def test_generate_vowel_one(self, mock_randint):
+        actual = generate_name(1)
+        expected = 'Ba'
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=0)
+    def test_generate_vowel_positive(self, mock_randint):
+        actual = generate_name(2)
+        expected = 'Baba'
+        self.assertEqual(expected, actual)

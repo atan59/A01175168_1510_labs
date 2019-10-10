ef4b637ccfe7590c2d7b4ee2311e4d14ed9ae8b1
atan59 <atan59@my.bcit.ca>
2019-10-10 13:19:03 -0700

    Added more unittests in test_choose_inventory.py to test printed output.

--- Lab05/test_choose_inventory.py
+++ Lab05/test_choose_inventory.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch
+import io
 from Lab05.lab05 import choose_inventory
 
 
@@ -10,14 +11,28 @@ class TestChooseInventory(TestCase):
         expected = []
         self.assertEqual(expected, actual)
 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_choose_inventory_selection_negative_stdout(self, mock_stdout):
+        choose_inventory(['Sword', 'Shield', "Bow"], -1)
+        actual = mock_stdout.getvalue()
+        expected = "You can't make a negative selection!\n"
+        self.assertEqual(expected, actual)
+
     @patch('random.choices', return_value=['Sword'])
-    def test_choose_inventory_selection_negative(self, mock_choices):
+    def test_choose_inventory_selection_negative_return_value(self, mock_choices):
         actual = choose_inventory(['Sword', 'Shield', "Bow"], -1)
         expected = []
         self.assertEqual(expected, actual)
 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_choose_inventory_selection_greater_than_list_length_stdout(self, mock_stdout):
+        choose_inventory(['Sword', 'Shield', "Bow"], 4)
+        actual = mock_stdout.getvalue()
+        expected = "Your selection is greater than the amount of items in the inventory."
+        self.assertEqual(expected, actual)
+
     @patch('random.choices', return_value=['Sword'])
-    def test_choose_inventory_selection_greater_than_list_length(self, mock_choices):
+    def test_choose_inventory_selection_greater_than_list_length_return_value(self, mock_choices):
         actual = choose_inventory(['Sword', 'Shield', "Bow"], 4)
         expected = ['Bow', 'Shield', 'Sword']
         self.assertEqual(expected, actual)

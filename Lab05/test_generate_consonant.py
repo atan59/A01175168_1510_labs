452c72ddacc5213d9e9fc36ff9f94e3b574b3466
atan59 <atan59@my.bcit.ca>
2019-10-10 00:11:15 -0700

    Completed unittest for generate_consonant function.

--- Lab05/test_generate_consonant.py
+++ Lab05/test_generate_consonant.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+from unittest.mock import patch
+from Lab05.lab05 import generate_consonant
 
 
-class TestGenerate_consonant(TestCase):
-    def test_generate_consonant(self):
-        self.fail()
+class TestGenerateConsonant(TestCase):
+    @patch('random.randint', return_value=0)
+    def test_generate_vowel_min(self, mock_randint):
+        actual = generate_consonant()
+        expected = 'b'
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=10)
+    def test_generate_vowel_mid(self, mock_randint):
+        actual = generate_consonant()
+        expected = 'n'
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=20)
+    def test_generate_vowel_max(self, mock_randint):
+        actual = generate_consonant()
+        expected = 'z'
+        self.assertEqual(expected, actual)

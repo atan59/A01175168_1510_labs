997ac625946814e15c5bed210e00691ca8f8ae5a
atan59 <atan59@my.bcit.ca>
2019-10-10 13:20:43 -0700

    Modified names of unittest functions for test_generate_consonant.py.

--- Lab05/test_generate_consonant.py
+++ Lab05/test_generate_consonant.py
@@ -5,19 +5,19 @@ from Lab05.lab05 import generate_consonant
 
 class TestGenerateConsonant(TestCase):
     @patch('random.randint', return_value=0)
-    def test_generate_vowel_min(self, mock_randint):
+    def test_generate_consonant_min(self, mock_randint):
         actual = generate_consonant()
         expected = 'b'
         self.assertEqual(expected, actual)
 
     @patch('random.randint', return_value=10)
-    def test_generate_vowel_mid(self, mock_randint):
+    def test_generate_consonant_mid(self, mock_randint):
         actual = generate_consonant()
         expected = 'n'
         self.assertEqual(expected, actual)
 
     @patch('random.randint', return_value=20)
-    def test_generate_vowel_max(self, mock_randint):
+    def test_generate_consonant_max(self, mock_randint):
         actual = generate_consonant()
         expected = 'z'
         self.assertEqual(expected, actual)

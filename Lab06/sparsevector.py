5ad209e35c7a7a0e005574e115b28be5045ab7f2
atan59 <atan59@my.bcit.ca>
2019-10-17 13:41:03 -0700

    Added docstring and doctest to sparse_add

--- Lab06/sparsevector.py
+++ Lab06/sparsevector.py
@@ -1,7 +1,21 @@
 """Calculate the sum of two vectors."""
+import doctest
 
 
 def sparse_add(sparse_vector_1, sparse_vector_2):
+    """
+    Calculate sum of the values of two sparse vector dictionaries.
+
+    :param sparse_vector_1: a dictionary
+    :param sparse_vector_2: a dictionary
+    :precondition: sparse_vector_1 must be a dictionary with no strings as values
+    :precondition: sparse_vector_2 must be a dictionary with no strings as values
+    :postcondition: calculate the sum of the values of the two sparse vector dictionaries
+    :return: a dictionary
+
+    >>> sparse_add({0: 1, 3: 5, 4: 2}, {0: 2, 3: 4, 4: 5})
+    {0: 3, 3: 9, 4: 7}
+    """
     sparse_vector_sum = {}
 
     for key in sparse_vector_1:
@@ -18,3 +32,7 @@ def sparse_add(sparse_vector_1, sparse_vector_2):
             del sparse_vector_sum[key]
 
     return sparse_vector_sum
+
+
+if __name__ == "__main__":
+    doctest.testmod()

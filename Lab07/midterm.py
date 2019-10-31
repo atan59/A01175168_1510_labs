b81fcae179ead696908634b45d89527075a33c71
atan59 <atan59@my.bcit.ca>
2019-10-31 08:33:53 -0700

    Implemented solution to list_tagger function.

--- Lab07/midterm.py
+++ Lab07/midterm.py
@@ -1,1 +1,21 @@
 """Implementation of solutions for the six coding questions from the midterm."""
+
+
+def list_tagger(batch):
+    """
+    Tag a list with its length at the beginning.
+
+    :param batch: a list
+    :precondition: batch must be a list
+    :postcondition: insert length of list at the beginning of the list
+    :return: a list
+
+    >>> list_tagger([])
+    [0]
+    >>> list_tagger([1, 2, 3])
+    [3, 1, 2, 3]
+    """
+    result = []
+    result.insert(0, len(batch))
+
+    return result

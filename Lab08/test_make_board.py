b68f2f498c2e8d3817e39eb4f7057549fa381990
atan59 <atan59@my.bcit.ca>
2019-11-04 17:02:00 -0800

    Completed unit test for make_board function.

--- Lab08/test_make_board.py
+++ Lab08/test_make_board.py
@@ -1,6 +1,22 @@
 from unittest import TestCase
+from Lab08.maze import make_board
 
 
 class TestMakeBoard(TestCase):
-    def test_make_board(self):
-        self.fail()
+    def test_make_board_correct_tuples(self):
+        actual = make_board()
+        expected = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+                    (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
+                    (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
+                    (3, 0), (3, 1), (3, 2), (3, 3), (3, 4),
+                    (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+        self.assertEqual(expected, actual)
+
+    def test_make_board_type(self):
+        actual = type(make_board())
+        expected = type([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+                         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
+                         (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
+                         (3, 0), (3, 1), (3, 2), (3, 3), (3, 4),
+                         (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)])
+        self.assertEqual(expected, actual)

5b77ab315c8b6f8c46cbcd8dfa2d61769e931e25
atan59 <atan59@my.bcit.ca>
2019-11-04 16:49:01 -0800

    Created functions for maze.py.

---
+++ Lab08/maze.py
@@ -0,0 +1,35 @@
+"""A simple 5x5 maze game."""
+import doctest
+
+
+def make_board():
+    pass
+
+
+def make_character():
+    pass
+
+
+def get_user_choice():
+    pass
+
+
+def validate_move(board, character, direction):
+    pass
+
+
+def move_character(character, direction):
+    pass
+
+
+def check_if_exit_reached(character):
+    pass
+
+
+def game():
+    pass
+
+
+if __name__ == "__main__":
+    # doctest.testmod()
+    # game()

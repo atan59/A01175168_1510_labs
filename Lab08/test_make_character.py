61c0243ada6377ed2ebd0e18666131c3fc0f5f4b
atan59 <atan59@my.bcit.ca>
2019-11-04 17:06:47 -0800

    Created unit test for make_character function.

--- Lab08/test_make_character.py
+++ Lab08/test_make_character.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
+from Lab08.maze import make_character
 
 
 class TestMakeCharacter(TestCase):
-    def test_make_character(self):
-        self.fail()
+    def test_make_character_correct_key(self):
+        actual = make_character().keys()
+        expected = "Current Position"
+        self.assertIn(expected, actual)
+
+    def test_make_character_correct_starting_value(self):
+        actual = make_character().values()
+        expected = (0, 0)
+        self.assertIn(expected, actual)
+
+    def test_make_character_correct_item(self):
+        actual = make_character().items()
+        expected = ("Current Position", (0, 0))
+        self.assertIn(expected, actual)
+
+    def test_make_character_type(self):
+        actual = type(make_character())
+        expected = type({"Current Position": (0, 0)})
+        self.assertEqual(expected, actual)

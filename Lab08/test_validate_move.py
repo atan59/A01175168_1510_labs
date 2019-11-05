446f685bef9c4b3f40418ea0c4fa47e4b62b9be9
atan59 <atan59@my.bcit.ca>
2019-11-04 17:20:40 -0800

    Created unit test for validate_move function.

--- Lab08/test_validate_move.py
+++ Lab08/test_validate_move.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
+from Lab08.maze import validate_move
 
 
 class TestValidateMove(TestCase):
-    def test_validate_move(self):
-        self.fail()
+    def test_validate_move_possible(self):
+        actual = validate_move([(0, 0), (0, 1), (1, 0), (1, 1)], {"Current Position": (0, 0)}, "2")
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_validate_move_impossible(self):
+        actual = validate_move([(0, 0), (0, 1), (1, 0), (1, 1)], {"Current Position": (0, 0)}, "1")
+        expected = False
+        self.assertEqual(expected, actual)
+
+    def test_validate_move_possible_type(self):
+        actual = type(validate_move([(0, 0), (0, 1), (1, 0), (1, 1)], {"Current Position": (0, 0)}, "2"))
+        expected = type(True)
+        self.assertEqual(expected, actual)
+
+    def test_validate_move_impossible_type(self):
+        actual = type(validate_move([(0, 0), (0, 1), (1, 0), (1, 1)], {"Current Position": (0, 0)}, "1"))
+        expected = type(False)
+        self.assertEqual(expected, actual)

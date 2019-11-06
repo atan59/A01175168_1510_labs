f96ef6c18e91bc0277f505902f58e550a0582b93
atan59 <atan59@my.bcit.ca>
2019-11-06 08:30:33 -0800

    Finished unit test for move_character function.

--- Lab08/test_move_character.py
+++ Lab08/test_move_character.py
@@ -1,6 +1,38 @@
 from unittest import TestCase
+from unittest.mock import patch
+from Lab08.maze import move_character
+import io
 
 
 class TestMoveCharacter(TestCase):
-    def test_move_character(self):
-        self.fail()
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_move_character_north(self, mock_stdout):
+        move_character({"Current Position": (0, 1)}, "1")
+        actual = mock_stdout.getvalue()
+        expected = "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n" + "Your current position (x, y) is: (0, 0)\n" +\
+                   "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n"
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_move_character_east(self, mock_stdout):
+        move_character({"Current Position": (0, 0)}, "2")
+        actual = mock_stdout.getvalue()
+        expected = "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n" + "Your current position (x, y) is: (1, 0)\n" + \
+                   "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n"
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_move_character_south(self, mock_stdout):
+        move_character({"Current Position": (0, 0)}, "3")
+        actual = mock_stdout.getvalue()
+        expected = "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n" + "Your current position (x, y) is: (0, 1)\n" + \
+                   "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n"
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_move_character_west(self, mock_stdout):
+        move_character({"Current Position": (1, 0)}, "4")
+        actual = mock_stdout.getvalue()
+        expected = "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n" + "Your current position (x, y) is: (0, 0)\n" + \
+                   "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n"
+        self.assertEqual(expected, actual)

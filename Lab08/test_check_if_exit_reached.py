83cc15c7b433bdc9bad333ada59d9d1028f75812
atan59 <atan59@my.bcit.ca>
2019-11-06 08:33:39 -0800

    Finished unit test for check_if_exit_reached function.

--- Lab08/test_check_if_exit_reached.py
+++ Lab08/test_check_if_exit_reached.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
+from Lab08.maze import check_if_exit_reached
 
 
 class TestCheckIfExitReached(TestCase):
-    def test_check_if_exit_reached(self):
-        self.fail()
+    def test_check_if_exit_reached_true(self):
+        actual = check_if_exit_reached({"Current Position": (4, 4)})
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_check_if_exit_reached_false(self):
+        actual = check_if_exit_reached({"Current Position": (0, 0)})
+        expected = False
+        self.assertEqual(expected, actual)
+
+    def test_check_if_exit_reached_true_type(self):
+        actual = type(check_if_exit_reached({"Current Position": (4, 4)}))
+        expected = type(True)
+        self.assertEqual(expected, actual)
+
+    def test_check_if_exit_reached_false_type(self):
+        actual = type(check_if_exit_reached({"Current Position": (0, 0)}))
+        expected = type(False)
+        self.assertEqual(expected, actual)

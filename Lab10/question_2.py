30ce34c13a592b014c33ea4a1a517a2f76f76c57
atan59 <atan59@my.bcit.ca>
2019-11-21 04:01:31 -0800

    Finished docstring and doctest for Question 2. Debugged gcd function.

--- Lab10/question_2.py
+++ Lab10/question_2.py
@@ -1,7 +1,29 @@
 """Calculate greatest common denominator of two numbers."""
+import doctest
 
 
-def gcd(a, b):
-    while y != 0:
-        (x, y) = (b, a % b)
-    return x
+def gcd(a: int, b: int) -> int:
+    """
+    Calculate greatest common denominator
+
+    :param a: an int
+    :param b: an int
+    :precondition: a must be an int
+    :precondition: b must be an int
+    :postcondition: calculate the greatest common denominator
+    :return: an int
+
+    >>> gcd(4, 2)
+    2
+    >>> gcd(2, 4)
+    2
+    >>> gcd(2, 0)
+    2
+    """
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+
+
+if __name__ == "__main__":
+    doctest.testmod()
